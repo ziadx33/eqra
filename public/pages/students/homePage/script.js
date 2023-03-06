0d15b0b092b2b5b08c2cbf878b21f45e5062ca18
Nader-SH <nadershak149@gmail.com>
2023-03-06 12:30:40 +0200

    create the tabs responsive web design

--- public/pages/students/homePage/script.js
+++ public/pages/students/homePage/script.js
@@ -1,58 +1,87 @@
-const tabLinks = document.querySelectorAll('.tab-link');
-const innerTabLinks = document.querySelectorAll('.inner-tab-link');
-const tabContents = document.querySelectorAll('.tab-content');
-const innerTabContents = document.querySelectorAll('.inner-tab-content');
+const tabLinks = document.querySelectorAll(".tab-link");
+const innerTabLinks = document.querySelectorAll(".inner-tab-link");
+const tabContents = document.querySelectorAll(".tab-content");
+const innerTabContents = document.querySelectorAll(".inner-tab-content");
 
-tabLinks.forEach(link => {
-  link.addEventListener('click', () => {
+const innerTabLinks2 = document.querySelectorAll(".inner-tab-link-2");
+const tabContents2 = document.querySelectorAll(".tab-content-2");
+const innerTabContents2 = document.querySelectorAll(".inner-tab-content-2");
+
+tabLinks.forEach((link) => {
+  link.addEventListener("click", () => {
     const tab = link.dataset.tab;
+    console.log(tab,"data");
     setActiveTab(tab);
+    setActiveInnerTab(tab);
+    setActiveInnerTab2(tab);
   });
 });
 
-innerTabLinks.forEach(link => {
-    link.addEventListener('click', () => {
-      const tab = link.dataset.tab;
-      console.log('clicked')
-      console.log(tab)
-      setActiveInnerTab(tab);
-    });
+innerTabLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    const tab = link.dataset.tab;
+    setActiveInnerTab(tab);
   });
-  
+});
+
+innerTabLinks2.forEach((link) => {
+  link.addEventListener("click", () => {
+    const tab = link.dataset.tab;
+    setActiveInnerTab2(tab);
+  });
+});
 
 function setActiveTab(tab) {
-  tabLinks.forEach(link => {
+  tabLinks.forEach((link) => {
     if (link.dataset.tab === tab) {
-      link.classList.add('active');
+      link.classList.add("active");
     } else {
-      link.classList.remove('active');
+      link.classList.remove("active");
     }
   });
-  tabContents.forEach(content => {
+  tabContents.forEach((content) => {
     if (content.dataset.tab === tab) {
-      content.classList.add('active');
+      content.classList.add("active");
     } else {
-      content.classList.remove('active');
+      content.classList.remove("active");
     }
   });
 }
 
 function setActiveInnerTab(tab) {
-    innerTabLinks.forEach(link => {
-        console.log(link.dataset.tab, tab)
-      if (link.dataset.tab === tab) {
-        link.classList.add('active');
-      } else {
-        link.classList.remove('active');
-      }
-    });
-    innerTabContents.forEach(content => {
-        console.log(content.dataset.tab, tab)
-      if (content.dataset.tab === tab) {
-        content.classList.add('active');
-      } else {
-        content.classList.remove('active');
-      }
-    });
-  }
-  
+  innerTabLinks.forEach((link) => {
+    console.log(link.dataset.tab, tab);
+    if (link.dataset.tab === tab) {
+      link.classList.add("active");
+    } else {
+      link.classList.remove("active");
+    }
+  });
+  innerTabContents.forEach((content) => {
+    console.log(content.dataset.tab, tab);
+    if (content.dataset.tab === tab) {
+      content.classList.add("active");
+    } else {
+      content.classList.remove("active");
+    }
+  });
+}
+
+function setActiveInnerTab2(tab) {
+  innerTabLinks2.forEach((link) => {
+    console.log(link.dataset.tab, tab);
+    if (link.dataset.tab === tab) {
+      link.classList.add("active");
+    } else {
+      link.classList.remove("active");
+    }
+  });
+  innerTabContents2.forEach((content) => {
+    console.log(content.dataset.tab, tab);
+    if (content.dataset.tab === tab) {
+      content.classList.add("active");
+    } else {
+      content.classList.remove("active");
+    }
+  });
+}
