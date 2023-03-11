83d92a24a0ad7039fe8183b97327c6481007ddee
Nader-SH <nadershak149@gmail.com>
2023-03-12 00:07:28 +0200

    Create All Tabs With Clear

--- public/pages/admin/Activities/script.js
+++ public/pages/admin/Activities/script.js
@@ -1,17 +1,89 @@
-const navBarShow = document.querySelector('.navBarShow');
-const burgerMune =document.querySelector('.burgerMune');
-const coloredBut = document.querySelectorAll('.coloredBut');
+const navBarShow = document.querySelector(".navBarShow");
+const burgerMune = document.querySelector(".burgerMune");
+const coloredBut = document.querySelectorAll(".coloredBut");
+const colordFillter = document.querySelectorAll(".colordFillter");
 
-coloredBut.forEach(tab => {
-  tab.addEventListener('click', () => {
+const videoTab = document.querySelector("#videoTab");
+const component = document.querySelector("#component");
+const contantContainer = document.querySelector("#contantContainer");
+const educationalActivities = document.querySelector("#educationalActivities");
+const readingChallenge = document.querySelector("#readingChallenge");
+
+let videos = [
+  {
+    src: "../../../assets/Document - Google Chrome 2023-03-07 23-42-36.mp4",
+    title: "عنوان الفيديو",
+    date: "12/12/2022",
+    viewers: 500,
+  },
+  {
+    src: "../../../assets/Document - Google Chrome 2023-03-07 23-42-36.mp4",
+    title: "عنوان الفيديو",
+    date: "12/12/2022",
+    viewers: 1000,
+  },
+  {
+    src: "../../../assets/Document - Google Chrome 2023-03-07 23-42-36.mp4",
+    title: "عنوان الفيديو",
+    date: "12/12/2022",
+    viewers: 750,
+  },
+  {
+    src: "../../../assets/Document - Google Chrome 2023-03-07 23-42-36.mp4",
+    title: "عنوان الفيديو",
+    date: "12/12/2022",
+    viewers: 1200,
+  },
+  {
+    src: "../../../assets/Document - Google Chrome 2023-03-07 23-42-36.mp4",
+    title: "عنوان الفيديو",
+    date: "12/12/2022",
+    viewers: 900,
+  },
+  {
+    src: "../../../assets/Document - Google Chrome 2023-03-07 23-42-36.mp4",
+    title: "عنوان الفيديو",
+    date: "12/12/2022",
+    viewers: 800,
+  },
+];
+
+window.onload = renderDataTabOne();
+educationalActivities.addEventListener("click", renderDataTabOne);
+function renderDataTabOne() {
+  if (!component.classList.contains("hidden")) {
+    component.classList.add("hidden");
+  }
+  getVideos(videos);
+}
+
+readingChallenge.addEventListener("click", () => {
+  if (component.classList.contains("hidden")) {
+    component.classList.remove("hidden");
+  }
+  videoTab.textContent = "";
+});
+
+colordFillter.forEach((tab) => {
+  tab.addEventListener("click", () => {
     // remove active class from all tabs
-    coloredBut.forEach(tab => tab.classList.remove('active'));
-    
+    colordFillter.forEach((tab) => tab.classList.remove("activeFillter"));
+
     // add active class to the clicked tab
-    tab.classList.add('active');
+    tab.classList.add("activeFillter");
   });
 });
-burgerMune.addEventListener('click',()=>{
+
+coloredBut.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    // remove active class from all tabs
+    coloredBut.forEach((tab) => tab.classList.remove("active"));
+
+    // add active class to the clicked tab
+    tab.classList.add("active");
+  });
+});
+burgerMune.addEventListener("click", () => {
   console.log("data");
 
   if (navBarShow.classList.contains("hidden")) {
@@ -19,8 +91,7 @@ burgerMune.addEventListener('click',()=>{
   } else {
     navBarShow.classList.add("hidden");
   }
-})
-
+});
 
 function showComponent(componentId) {
   // Hide all components
@@ -34,4 +105,88 @@ function showComponent(componentId) {
   selectedComponent.classList.remove("hidden");
 }
 
+function getVideos(videos) {
+  videos.forEach(function (video) {
+    let videoCard = document.createElement("div");
+    videoCard.classList.add(
+      "p-2",
+      "m-2",
+      "drop-shadow",
+      "bg-white",
+      "rounded-lg",
+      "max-w-sm"
+    );
 
+    let videoEl = document.createElement("video");
+    videoEl.src = video.src;
+    videoEl.classList.add("object-cover", "videoCss");
+    const titleAndviewers = document.createElement("div");
+    titleAndviewers.classList.add("flex", "justify-between");
+    let title = document.createElement("h3");
+    title.textContent = video.title;
+    title.classList.add("text-lg", "font-bold", "m-4", "text-purple-800");
+    const dataAndTitleDate = document.createElement("div");
+    dataAndTitleDate.classList.add("flex", "justify-between", "m-4");
+    const titleData = document.createElement("p");
+    titleData.textContent = "تاريخ النشر";
+    let date = document.createElement("p");
+    date.textContent = video.date;
+    date.classList.add("text-sm", "mt-1", "font-medium");
+    const theViewersAndicon = document.createElement("div");
+    theViewersAndicon.classList.add("m-4");
+    let viewersIcon = document.createElement("img");
+    viewersIcon.src = "../../../assets/eye.png";
+
+    viewersIcon.classList.add(
+      "w-4",
+      "h-4",
+      "inline-block",
+      "mr-1",
+      "align-middle",
+      "m-2"
+    );
+
+    let viewersCount = document.createElement("span");
+    viewersCount.textContent = video.viewers.toLocaleString();
+    viewersCount.classList.add("text-sm", "align-middle", "text-gray-400");
+
+    let playIcon = document.createElement("img");
+    playIcon.src = "../../../assets/paly.png";
+    playIcon.classList.add(
+      "absolute",
+      "top-1/3",
+      "left-1/2",
+      "transform",
+      "-translate-x-1/2",
+      "-translate-y-1/2"
+    );
+
+    let isPlaying = false;
+    playIcon.addEventListener("click", function () {
+      if (!isPlaying) {
+        videoEl.play();
+        playIcon.style.display = "none";
+        isPlaying = true;
+        console.log("start");
+      }
+    });
+    videoEl.addEventListener("click", function () {
+      videoEl.pause();
+      playIcon.style.display = "block";
+      isPlaying = false;
+    });
+
+    videoCard.appendChild(videoEl);
+    theViewersAndicon.appendChild(viewersCount);
+    theViewersAndicon.appendChild(viewersIcon);
+    titleAndviewers.appendChild(theViewersAndicon);
+    titleAndviewers.appendChild(title);
+    videoCard.appendChild(titleAndviewers);
+    dataAndTitleDate.appendChild(date);
+    dataAndTitleDate.appendChild(titleData);
+    videoCard.appendChild(dataAndTitleDate);
+    videoCard.appendChild(playIcon);
+
+    videoTab.appendChild(videoCard);
+  });
+}
--- public/pages/admin/Users/script.js
+++ public/pages/admin/Users/script.js
@@ -1,19 +1,80 @@
-const navBarShow = document.querySelector('.navBarShow');
-const burgerMune =document.querySelector('.burgerMune');
-const coloredBut = document.querySelectorAll('.coloredBut');
+const navBarShow = document.querySelector(".navBarShow");
+const burgerMune = document.querySelector(".burgerMune");
+const coloredBut = document.querySelectorAll(".coloredBut");
 
-coloredBut.forEach(tab => {
-  tab.addEventListener('click', () => {
+const contantContainer = document.querySelector("#contantContainer");
+const buttonFetch = document.querySelector("#buttonFetch");
+const admin = document.querySelector("#admin");
+const students = document.querySelector("#students");
+const teachers = document.querySelector("#teachers");
+const rows = document.querySelector("#rows");
+const seasons = document.querySelector("#seasons");
+window.onload = renderDataTabOne();
+admin.addEventListener("click", renderDataTabOne);
+function renderDataTabOne() {
+  contantContainer.textContent = "admin";
+  buttonFetch.textContent = "";
+  const adminButton = document.createElement("button");
+  adminButton.classList.add("cursor-pointer", "p-1");
+  adminButton.textContent = "اضافة مشرف";
+  adminButton.id = "addAdmin";
+  adminButton.onclick = addAdminButton;
+  buttonFetch.appendChild(adminButton);
+}
+
+students.addEventListener("click", () => {
+  contantContainer.textContent = "students";
+  buttonFetch.textContent = "";
+  const studentsButton = document.createElement("button");
+  studentsButton.classList.add("cursor-pointer", "p-1");
+  studentsButton.textContent = "اضافة طالب";
+  studentsButton.id = "addStudents";
+  studentsButton.onclick = addStudentsButton;
+  buttonFetch.appendChild(studentsButton);
+});
+
+teachers.addEventListener("click", () => {
+  contantContainer.textContent = "teachers";
+  buttonFetch.textContent = "";
+  const teachersButton = document.createElement("button");
+  teachersButton.classList.add("cursor-pointer", "p-1");
+  teachersButton.textContent = "اضافة معلم";
+  teachersButton.id = "addTeachers";
+  teachersButton.onclick = addTeachersButton;
+  buttonFetch.appendChild(teachersButton);
+});
+rows.addEventListener("click", () => {
+  contantContainer.textContent = "rows";
+  buttonFetch.textContent = "";
+  const rowsButton = document.createElement("button");
+  rowsButton.classList.add("cursor-pointer", "p-1");
+  rowsButton.textContent = "اضافة صف";
+  rowsButton.id = "addRows";
+  rowsButton.onclick = addRowsButton;
+  buttonFetch.appendChild(rowsButton);
+});
+seasons.addEventListener("click", () => {
+  contantContainer.textContent = "seasons";
+  buttonFetch.textContent = "";
+  const seasonsButton = document.createElement("button");
+  seasonsButton.classList.add("cursor-pointer", "p-1");
+  seasonsButton.textContent = "اضافة فصل";
+  seasonsButton.id = "addRows";
+  seasonsButton.onclick = addSeasonsButton;
+  buttonFetch.appendChild(seasonsButton);
+});
+
+coloredBut.forEach((tab) => {
+  tab.addEventListener("click", () => {
     // remove active class from all tabs
-    coloredBut.forEach(tab => tab.classList.remove('active'));
-    
+    coloredBut.forEach((tab) => tab.classList.remove("active"));
+
     // add active class to the clicked tab
-    tab.classList.add('active');
+    tab.classList.add("active");
   });
 });
 
-
-burgerMune.addEventListener('click',()=>{
+burgerMune.addEventListener("click", () => {
   console.log("data");
 
   if (navBarShow.classList.contains("hidden")) {
@@ -21,8 +82,7 @@ burgerMune.addEventListener('click',()=>{
   } else {
     navBarShow.classList.add("hidden");
   }
-})
-
+});
 
 function showComponent(componentId) {
   // Hide all components
@@ -36,4 +96,18 @@ function showComponent(componentId) {
   selectedComponent.classList.remove("hidden");
 }
 
-window.onload = showComponent("component2");
+function addAdminButton() {
+  console.log("addAdminButton");
+}
+function addStudentsButton() {
+  console.log("addStudentsButton");
+}
+function addTeachersButton() {
+  console.log("addTeachersButton");
+}
+function addRowsButton() {
+  console.log("addRowsButton");
+}
+function addSeasonsButton() {
+  console.log("addSeasonsButton");
+}
