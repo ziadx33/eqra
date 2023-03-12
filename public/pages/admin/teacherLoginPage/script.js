0a622315e209c0b8f56a1def2b720650deadcb75
mahmoud <mahmoudhammad3009@gmail.com>
2023-03-12 04:15:34 +0200

    create all forms for manager

---
+++ public/pages/admin/teacherLoginPage/script.js
@@ -0,0 +1,60 @@
+const forGetPassword = document.querySelector('.forGetPassword');
+const closeIcon = document.querySelector('.closeIcon');
+const modal = document.querySelector('.modal');
+
+
+forGetPassword.addEventListener('click', () => {
+  modal.classList.remove('hidden');
+});
+
+closeIcon.addEventListener('click', () => {
+  modal.classList.add('hidden');
+});
+
+const form = document.getElementById('userForm');
+
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const userName = document.getElementById('userName').value;
+  const password = document.getElementById('password').value;
+
+  // Validate user name
+  if (userName === '') {
+    document.getElementById('userNameError').textContent =
+      'الرجاء إدخال إسم المستخدم';
+  } else if (!/^[a-zA-Z]*$/.test(userName)) {
+    document.getElementById('userNameError').textContent =
+      'يجب أن يحتوي إسم المستخدم على الأحرف الإنجليزية فقط بدون مسافات';
+  } else {
+    document.getElementById('userNameError').textContent = '';
+  }
+
+  // Validate password
+  if (password === '') {
+    document.getElementById('passwordError').textContent =
+      'الرجاء إدخال كلمة المرور';
+  } else {
+    document.getElementById('passwordError').textContent = '';
+  }
+
+
+
+
+
+
+  // Submit the form if there are no errors
+  if (
+    document.getElementById('userNameError').textContent === '' &&
+    document.getElementById('passwordError').textContent === ''
+  ) {
+    // get the form data
+    const formData = new FormData(event.target);
+
+    // iterate over the form data and log each field to the console
+    for (const [name, value] of formData) {
+      console.log(`${name}: ${value}`);
+    }
+  }
+}
