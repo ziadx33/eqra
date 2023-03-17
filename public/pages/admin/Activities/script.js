372e26d22b46cf962db79a0c2ee45a53db39fd96
Nader-SH <nadershak149@gmail.com>
2023-03-17 18:35:47 +0200

    last Page updata

--- public/pages/admin/Activities/script.js
+++ public/pages/admin/Activities/script.js
@@ -51,12 +51,6 @@ const videos = [
 window.onload = renderDataTabOne();
 function renderDataTabOne() {
   videoTab.textContent = "";
-  if (!component.classList.contains("hidden")) {
-    component.classList.add("hidden");
-  }
-  while (contantContainer.firstChild) {
-    contantContainer.removeChild(contantContainer.firstChild);
-  }
   getVideos(videos);
 }
 
@@ -176,10 +170,11 @@ function getVideos(videos) {
 // Respon the form
 function videoForm(video) {
   tabContent.classList.add("hidden");
+  vedioForm.classList.remove("hidden");
   console.log(video);
   // JavaScript code to create the form using the DOM
   const form = document.createElement("form");
-  form.classList.add("max-w-md", "mx-auto", "mt-4", "w-full");
+  form.classList.add("mx-auto", "mt-4", "w-full");
   form.setAttribute("dir", "rtl");
   form.setAttribute("lang", "ar");
 
@@ -189,16 +184,16 @@ function videoForm(video) {
   const label = document.createElement("label");
   label.setAttribute("for", "departmentName");
   label.classList.add("font-bold");
-  label.textContent = "قسم جديد";
+  label.textContent = "عنوان النشاط";
 
   const input = document.createElement("input");
   input.setAttribute("type", "text");
   input.setAttribute("name", "departmentName");
-  input.setAttribute("placeholder", "اسم القسم");
-  input.dir='rtl';
+  input.setAttribute("placeholder", "عنوان النشاط");
+  input.dir = "rtl";
   input.classList.add(
     "mt-3",
-    "py-3",
+    "py-2",
     "bg-gray-light",
     "w-full",
     "rounded-md",
@@ -209,10 +204,151 @@ function videoForm(video) {
     "sm:text-sm",
     "sm:leading-6"
   );
+  const videoCard = document.createElement("div");
+  videoCard.classList.add(
+    "p-2",
+    "m-2",
+    "drop-shadow",
+    "bg-white",
+    "rounded-lg",
+    "flex",
+    "justify-center"
+  );
 
+  const videoEl = document.createElement("video");
+  videoEl.src = video.src;
+  videoEl.classList.add("object-cover", "videoCss");
+  const playIcon = document.createElement("img");
+  playIcon.src = "../../../assets/paly.png";
+  playIcon.classList.add(
+    "absolute",
+    "top-1/2",
+    "left-1/2",
+    "transform",
+    "-translate-x-1/2",
+    "-translate-y-1/2"
+  );
+
+  let isPlaying = false;
+  playIcon.addEventListener("click", function () {
+    if (!isPlaying) {
+      videoEl.play();
+      playIcon.style.display = "none";
+      isPlaying = true;
+      console.log("start");
+    }
+  });
+  videoEl.addEventListener("click", function () {
+    videoEl.pause();
+    playIcon.style.display = "block";
+    isPlaying = false;
+  });
+  videoCard.appendChild(videoEl);
+  videoCard.appendChild(playIcon);
+
+
+  const divButtons = document.createElement("div");
+  divButtons.classList.add(
+    "justify-center",
+    "flex",
+    "flex-wrap",
+    "md:justify-around",
+    "lg:justfiy-around"
+  );
+
+  const recInput = document.createElement("input");
+  recInput.type = "file";
+  recInput.accept = "*";
+  recInput.id = "recInput";
+  recInput.hidden = true; // hide the input element
+  // recInput.addEventListener("change", handleRecInputChange); // add a change event listener to handle image selection
+
+  const recLabel = document.createElement("label");
+  recLabel.textContent = "إضافة من الجهاز";
+  recLabel.dir = "rtl";
+  recLabel.classList.add(
+    "m-2",
+    "px-8",
+    "py-2",
+    "md:px-10",
+    "md:py-2",
+    "lg:px-10",
+    "lg:py-2",
+    "text-white",
+    "rounded-lg",
+    "bg-purple-light",
+    "border-none",
+    "focus:outline-none",
+    "whitespace-nowrap",
+    "flex"
+  );
+  recLabel.htmlFor = "recInput";
+  const recLabelImage = document.createElement("img");
+  recLabelImage.src = "../../../assets/cameraIcon.png";
+  recLabel.appendChild(recLabelImage);
+
+  const uploadInput = document.createElement("input");
+  uploadInput.type = "file";
+  uploadInput.accept = "image/*";
+  uploadInput.id = "uploadInput";
+  uploadInput.hidden = true; // hide the input element
+  // uploadInput.addEventListener("change", handleUploadInputChange); // add a change event listener to handle image selection
+
+  const uploadLabel = document.createElement("label");
+  uploadLabel.textContent = "تسجيل من الجهاز";
+  uploadLabel.dir = "rtl";
+  uploadLabel.classList.add(
+    "m-2",
+    "px-8",
+    "py-2",
+    "md:px-10",
+    "md:py-2",
+    "lg:px-10",
+    "lg:py-2",
+    "text-white",
+    "rounded-lg",
+    "bg-purple-light",
+    "border-none",
+    "focus:outline-none",
+    "whitespace-nowrap",
+    "flex"
+  );
+  uploadLabel.htmlFor = "uploadInput";
+  const uploadLabelImage = document.createElement("img");
+  uploadLabelImage.src = "../../../assets/uploadIcon.png";
+  uploadLabel.appendChild(uploadLabelImage);
+
+  divButtons.appendChild(recInput);
+  divButtons.appendChild(recLabel);
+  divButtons.appendChild(uploadInput);
+  divButtons.appendChild(uploadLabel);
+  const divPostBut = document.createElement("div");
+  divPostBut.classList.add("mt-2", "flex", "justify-center");
+  const postBut = document.createElement("button");
+  postBut.textContent = "نشر";
+  postBut.classList.add(
+    "mt-2",
+    "px-8",
+    "py-2",
+    "md:px-10",
+    "md:py-2",
+    "lg:px-10",
+    "lg:py-2",
+    "text-white",
+    "rounded-lg",
+    "bg-dark-green",
+    "border-none",
+    "focus:outline-none",
+    "whitespace-nowrap",
+    "flex"
+  );
+  divPostBut.appendChild(postBut);
   div.appendChild(label);
   div.appendChild(input);
   form.appendChild(div);
 
   vedioForm.appendChild(form);
+  vedioForm.appendChild(videoCard);
+  vedioForm.appendChild(divButtons);
+  vedioForm.appendChild(divPostBut);
 }
